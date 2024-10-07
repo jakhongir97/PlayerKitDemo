ccf510f6436ba60f9c7651ff007fb1cd9cb586e9
Jakhon Nematov <nj@exante.eu>
2024-10-07 12:40:16 +0500

    Initial Commit

---
+++ PlayerKitDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayerKitDemo
+//
+//  Created by Jakhongir Nematov on 07/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayerKitDemo/PlayerKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerKitDemoApp.swift
+//  PlayerKitDemo
+//
+//  Created by Jakhongir Nematov on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayerKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
