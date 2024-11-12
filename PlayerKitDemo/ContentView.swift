97c3447edc1c3c6fba22b0a8e76ea10e19af4aae
Jakhon Nematov <nj@exante.eu>
2024-11-12 15:48:24 +0500

    Lock Button

--- PlayerKitDemo/ContentView.swift
+++ PlayerKitDemo/ContentView.swift
@@ -3,7 +3,7 @@ import PlayerKit
 
 struct ContentView: View {
     // Use your real HLS URL
-    let videoURL = URL(string: "https://st1.itv.uz/2/hls/map/ftp15/2023/vod/k/l/0c5b99cc49a587aed38d2a8a66c131bd/master.m3u8?token=wVoiVmnndOUfmU-RlaYkng&e=1731341623&traffic=0&uid=3411046&device=IPADOS&ip=10.32.120.2&mode=mapped")!
+    let videoURL = URL(string: "https://ftp18m.cinerama.uz/hls/converted/20241001/647c3f8b1002351900d10e6df3794b40.mp4/playlist.m3u8?token=3IfdgHsQXOILgSRdQjo5_w&expires=1729107992&ip=10.32.120.2")!
 
     var body: some View {
         PlayerView()
