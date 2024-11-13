b8bfd3550e12b3e1a1ed31e22f0deb69a7ff65e4
Jakhon Nematov <nj@exante.eu>
2024-11-13 16:10:57 +0500

    Minor fix

--- PlayerKitDemo/ContentView.swift
+++ PlayerKitDemo/ContentView.swift
@@ -6,10 +6,7 @@ struct ContentView: View {
     let videoURL = URL(string: "https://ftp18m.cinerama.uz/hls/converted/20241001/647c3f8b1002351900d10e6df3794b40.mp4/playlist.m3u8?token=3IfdgHsQXOILgSRdQjo5_w&expires=1729107992&ip=10.32.120.2")!
 
     var body: some View {
-        PlayerView()
-            .onAppear {
-                PlayerManager.shared.load(url: videoURL)
-            }
+        PlayerView(url: videoURL)
     }
 }
 
